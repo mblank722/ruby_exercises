bef870edcf9572a70f5c9f5ed91bf265151a63d6
mblank722 <mitchellblank@sbcglobal.net>
2017-02-08 11:25:18 -0800

    hashses has_value?

--- hashes.py
+++ hashes.py
@@ -8,6 +8,10 @@ hash1 = {key1: "val1", key2: "val2", key3:"val3", key4:  "val4"}
 # puts hash2.empty?
 # puts hash1.empty?
 
-puts hash1.has_key?('x')
-puts hash1.has_key?('key3')
-puts hash1.has_key?(:key3)
+# puts hash1.has_key?('x')
+# puts hash1.has_key?('key3')
+# puts hash1.has_key?(:key3)
+
+puts hash1.has_value?(1)
+puts hash1.has_value?("val5")
+puts hash1.has_value?("val4")
