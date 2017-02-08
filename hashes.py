c435c2b6f9e2f5aaf92c7d2d2c4fd36e7ca53439
mblank722 <mitchellblank@sbcglobal.net>
2017-02-08 11:22:36 -0800

    hashses has_key

--- hashes.py
+++ hashes.py
@@ -1,5 +1,13 @@
 hash1 = {key1: "val1", key2: "val2", key3:"val3", key4:  "val4"}
 
-puts hash1
-hash1.delete(:key2)
-puts hash1
+# puts hash1
+# hash1.delete(:key2)
+# puts hash1
+
+# hash2 = {}
+# puts hash2.empty?
+# puts hash1.empty?
+
+puts hash1.has_key?('x')
+puts hash1.has_key?('key3')
+puts hash1.has_key?(:key3)
