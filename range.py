8e1b4cc3a36b0eaa4c1e254e8dc461fe18b6c58d
mblank722 <mitchellblank@sbcglobal.net>
2017-02-08 10:59:18 -0800

    rnage.py v 2

--- range.py
+++ range.py
@@ -1,8 +1,18 @@
 arr = ["a", "b", "c", "d", "e"]
 # puts arr.include?("a")
 # puts arr.include?("z")
-#
-#include? = alias/synonym for member
 
-puts arr.member?("a")
-puts arr.member?("z")
+#include? = alias/synonym for member
+# puts arr.member?("a")
+# puts arr.member?("z")
+
+#last returns last value in range
+# puts arr.last()
+# puts (1..5).last
+# puts (1.5..6).last
+
+
+#first returns first value in range 
+# puts (1.5..6).first
+# puts (1...5).first
+# puts (1..5).first
