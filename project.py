2abd1d08fb58eddc3617382686a2127070f35e1f
mblank722 <mitchellblank@sbcglobal.net>
2017-02-09 12:20:05 -0800

    project fixed

--- project.py
+++ project.py
@@ -1,1 +1,19 @@
-paoc
+ # Project should have a name and description
+ # Project should have a method called elevator pitch
+
+class Project
+    attr_accessor  :p_name, :p_desc
+    def initialize (p_name, p_desc)
+        @p_name = p_name
+        @p_desc = p_desc
+        return self
+    end
+
+    def elevator_pitch()
+        print "Project - Name: ",@p_name, "  ,Description: ", @p_desc, "\n"
+        return self
+    end
+end
+project1 = Project.new("Project 1", "Description 1")
+puts project1.p_name #prints out Project 1
+project1.elevator_pitch #prints out 'Project 1, Description 1"
